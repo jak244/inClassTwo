e136b35dd3ec22c87a782f7bb7ad8ae2dc14d84b
Justin Keenan <jak244@pitt.edu>
2017-03-28 20:44:51 -0400

    Original Email to you failed

---
+++ inClassTwo.js
@@ -0,0 +1,23 @@
+function jCrimes() {
+  alert("Wanted for:\nMurder\nTerrorism\nForcing People to Laugh");
+}
+
+function hCrimes() {
+  alert("Wanted for:\nMurder\nTerrorism\nHitting People with a Mallot");
+}
+
+function jSeen() {
+  document.getElementById('who').innerHTML = "Joker";
+}
+
+function hSeen() {
+  document.getElementById('who').innerHTML = "Harley";
+}
+
+function bSeen() {
+  document.getElementById('who').innerHTML = "Both";
+}
+
+function submit() {
+  location.reload();
+}
